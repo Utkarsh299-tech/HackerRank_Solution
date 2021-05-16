541e433e9cdc72970787faa7c5f0a82b1d81afe8
Utkarsh Singh <singhutkarsh2799@gmail.com>
2021-05-17 00:32:18 +0530

    Create solution.java

---
+++ Java/Introduction/Java Stdin and Stdout I/solution.java
@@ -0,0 +1,19 @@
+// github.com/Utkarsh299-tech
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        
+ 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
