6a3a8b90e3b2dd42c75ed88d4c1d7b1f58c94630
Utkarsh Singh <singhutkarsh2799@gmail.com>
2021-06-24 10:40:37 +0530

    Create solution.java

---
+++ Java/Strings/Java Substring/solution.java
@@ -0,0 +1,17 @@
+// https://github.com/Utkarsh299-tech
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));    // This will print the substring from index start to end - 1
+    }
+}
